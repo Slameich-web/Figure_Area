cf3f3c68f60c18fa96e56d0b8da27044de2d0729
Прохоров Александр <slameacer@mail.ru>
2021-05-20 06:07:31 +0300

    Добавьте файлы проекта.

---
+++ MyLibrary/MyLibrary.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class MyLibrary
+    {
+
+
+        // Метод для площади круга
+
+        public double Area(double firstValue)
+        {
+            if (firstValue <= 0)
+            {
+                return 0;
+            }
+
+            return Math.Round(((firstValue * firstValue) * 3.14),2);
+
+        }
+
+        // Метод для площади треугольника
+
+        public double Area(double firstValue, double SecondValue, double ThridValue)
+        {
+            if (firstValue <= 0 || SecondValue <= 0 || ThridValue <= 0)
+            {
+                return 0;
+            }
+
+            double result = 0;
+            double p = (firstValue + SecondValue + ThridValue) / 2; //Находим полупериметр
+            int flag = 0; // 
+            bool isoscelesTriangle = false; // Результат проверки на равнобедренность треугольника
+            double maxValue = -1; /*Для наибольшой стороны
+                                   Использую -1 так как длина стороны треугольника не может быть отрицательной*/
+
+            //Находим MaxValue
+
+            if (firstValue > SecondValue && firstValue > ThridValue)
+            {
+                maxValue = firstValue;
+                flag = 1;
+                    
+            }
+            else if(SecondValue > firstValue && SecondValue > ThridValue)
+            {
+                flag = 2;
+                maxValue = SecondValue;
+            }
+            else if (ThridValue > firstValue && ThridValue > SecondValue)
+            {
+                flag = 3;
+                maxValue = ThridValue;
+            }
+
+            //Провереям треугольник на равнобедренность
+
+            if (flag == 3)
+            {
+                isoscelesTriangle = ((maxValue*maxValue) == (SecondValue * SecondValue) + (firstValue * firstValue));
+            }
+            if (flag == 2)
+            {
+                isoscelesTriangle = ((maxValue * maxValue) == (ThridValue * ThridValue) + (firstValue * firstValue));
+            }
+            if (flag == 1)
+            {
+                isoscelesTriangle = ((maxValue * maxValue) == (SecondValue * SecondValue) + (ThridValue * ThridValue));
+            }
+
+            //Выводим результат проверки на равнобедренность(В ТЗ нет информации что делать с проверкой, поэтому консоль)
+            if (isoscelesTriangle)
+            {
+                Console.WriteLine("Треугольник равнобедренный");
+            }
+            else
+            {
+                Console.WriteLine("Треугольник не равнобедренный");
+            }
+            result = Math.Round(Math.Sqrt(p * (p - firstValue) * (p - SecondValue) * (p - ThridValue)), 2);
+            if(Double.IsNaN(result))
+            {
+                return result = 0;
+            }
+            return result;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MyLibrary.Tests
+{
+    [TestClass]
+    public class MyLibraryTest
+    {
+        [TestMethod]
+        public void Area_Circle_10()
+        {
+            double x = 10;
+            double expected = 314;
+
+            MyLibrary c = new MyLibrary();
+            double actual = c.Area(x);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Area_Circle_0()
+        {
+            double x = 0;
+            double expected = 0;
+
+            MyLibrary c = new MyLibrary();
+            double actual = c.Area(x);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Area_Circle_negative_10()
+        {
+            double x = -10;
+            double expected = 0;
+
+            MyLibrary c = new MyLibrary();
+            double actual = c.Area(x);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Area_Triangle_3_4_5()
+        {
+            double x = 3;
+            double y = 4;
+            double z = 5;
+            double expected = 6;
+
+            MyLibrary a = new MyLibrary();
+            double actual = a.Area(x,y,z);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Area_Triangle_0_4_5()
+        {
+            double x = 0;
+            double y = 4;
+            double z = 5;
+            double expected = 0;
+
+            MyLibrary a = new MyLibrary();
+            double actual = a.Area(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Area_Triangle_negative_3_4_5()
+        {
+            double x = -3;
+            double y = 4;
+            double z = 5;
+            double expected = 0;
+
+            MyLibrary a = new MyLibrary();
+            double actual = a.Area(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Area_Triangle_9_4_5()
+        {
+            double x = 9;
+            double y = 4;
+            double z = 5;
+            double expected = 0;
+
+            MyLibrary a = new MyLibrary();
+            double actual = a.Area(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Area_Triangle_4_4_5()
+        {
+            double x = 4;
+            double y = 4;
+            double z = 5;
+            double expected = 7.81;
+
+            MyLibrary a = new MyLibrary();
+            double actual = a.Area(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Area_Triangle_100_4_5()
+        {
+            double x = 100;
+            double y = 4;
+            double z = 5;
+            double expected = 0;
+
+            MyLibrary a = new MyLibrary();
+            double actual = a.Area(x, y, z);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
